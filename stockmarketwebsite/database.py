8438a9999fcddb3a5a5ac50e8be61249af135cc3
Tintin <33134900+SamrathPalSingh@users.noreply.github.com>
2020-05-06 01:04:22 +0530

    migration

--- stockmarketwebsite/database.py
+++ stockmarketwebsite/database.py
@@ -7,12 +7,12 @@
 # from stock_prediction_logic.test import start_analysis
 # start_analysis()
 # import pandas as pd
-# data = pd.read_csv("C:\\Users\\BEST BUY\\Documents\\stocks_names.csv")
-# symbols = data.iloc[:,0:2].values
-# for i in symbols:
-    # record = Stock(stockName = i[1], stockSymbol = i[0], s_and_r_trend = "n", volume_trend='n', macd_trend = "na", volume = "-", candle_pattern = 'na', candle_trend='na', rank=int(0))
-    # record.save()
-    # print(i[0] + " | " + i[1])
+data = pd.read_csv("C:\\Users\\BEST BUY\\Documents\\stocks_names.csv")
+symbols = data.iloc[:,0:2].values
+for i in symbols:
+    record = Stock(stockName = i[1], stockSymbol = i[0], s_and_r_trend = "n", volume_trend='n', macd_trend = "na", volume = "-", candle_pattern = 'na', candle_trend='na', rank=int(0))
+    record.save()
+    print(i[0] + " | " + i[1])
 
 # import threading
 # from queue import Queue
