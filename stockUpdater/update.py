c65f89058f53353404641b49e6f581e1452f188a
Tintin <33134900+SamrathPalSingh@users.noreply.github.com>
2020-05-07 02:51:30 +0530

    TEST

--- stockUpdater/update.py
+++ stockUpdater/update.py
@@ -1,9 +1,9 @@
 from .stock_prediction_logic.analyse import start_analysis
-from home.models import stocks
+from home.models import stock
 
 def updateStocks():
     # start_analysis()
-    obj = stocks.objects.get(stockSymbol = "AAPL")
+    obj = stock.objects.get(stockSymbol = "AAPL")
     obj.macd_trend = 'be'
     obj.rank = int(4)
     import datetime
--- stockUpdater/updater.py
+++ stockUpdater/updater.py
@@ -4,5 +4,5 @@ from stockUpdater import update
 def start():
     scheduler = BackgroundScheduler()
     #scheduler.add_job(update.updateStocks, 'interval', minutes=1)
-    scheduler.add_job(update.updateStocks, 'cron', day_of_week='mon-fri', hour=2, minute=51)
+    scheduler.add_job(update.updateStocks, 'cron', day_of_week='mon-fri', hour=2, minute=53)
     scheduler.start()
