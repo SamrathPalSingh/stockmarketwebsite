093c1fb59791057cbcf72c99b25576d75624ee7e
Tintin <33134900+SamrathPalSingh@users.noreply.github.com>
2020-05-07 02:49:37 +0530

    TEST

--- stockUpdater/update.py
+++ stockUpdater/update.py
@@ -1,8 +1,12 @@
 from .stock_prediction_logic.analyse import start_analysis
 from home.models import stocks
+
 def updateStocks():
     # start_analysis()
-    obj = stock.objects.get(stockSymbol = "AAPL")
+    obj = stocks.objects.get(stockSymbol = "AAPL")
     obj.macd_trend = 'be'
     obj.rank = int(4)
+    import datetime
+    now = datetime.datetime.now()
+    obj.volume = str(now)
     obj.save()
