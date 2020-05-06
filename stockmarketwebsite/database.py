ecc9e6074a1050b40988907f35532e562310061c
Tintin <33134900+SamrathPalSingh@users.noreply.github.com>
2020-05-07 01:25:40 +0530

    database operations

--- stockmarketwebsite/database.py
+++ stockmarketwebsite/database.py
@@ -1,18 +1,18 @@
-# import os, sys
-# sys.path.append('C:/Users/BEST BUY/Documents/django/stockmarketwebsite/')
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'stockmarketwebsite.settings'
-# import django
-# django.setup()
-# from homepage.models import Stock
+import os, sys
+sys.path.append('C:/Users/BEST BUY/Documents/django/stockmarketwebsite/')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'stockmarketwebsite.settings'
+import django
+django.setup()
+from home.models import stock
 # from stock_prediction_logic.test import start_analysis
 # start_analysis()
-# import pandas as pd
-# data = pd.read_csv("C:\\Users\\BEST BUY\\Documents\\stocks_names.csv")
-# symbols = data.iloc[:,0:2].values
-# for i in symbols:
-#     record = Stock(stockName = i[1], stockSymbol = i[0], s_and_r_trend = "n", volume_trend='n', macd_trend = "na", volume = "-", candle_pattern = 'na', candle_trend='na', rank=int(0))
-#     record.save()
-#     print(i[0] + " | " + i[1])
+import pandas as pd
+data = pd.read_csv("stocks_names.csv")
+symbols = data.iloc[:,0:2].values
+for i in symbols:
+    record = stock(stockName = i[1], stockSymbol = i[0], s_and_r_trend = "n", volume_trend='n', macd_trend = "na", volume = "-", candle_pattern = 'na', candle_trend='na', rank=int(0))
+    record.save()
+    print(i[0] + " | " + i[1])
 
 # import threading
 # from queue import Queue
